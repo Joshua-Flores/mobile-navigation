0e279fe51a0b553538440dded65ad7290a55880c
Joshua Flores <joshuaflores@joshuas-mbp.myfiosgateway.com>
2021-06-22 21:11:05 -0400

    completed animation exercise

--- src/index.js
+++ src/index.js
@@ -2,66 +2,52 @@ import anime from 'animejs/lib/anime.es';
 
 document.querySelector('.button-nav').addEventListener('click', () => {
   const nav = document.querySelector('nav');
-  nav.classList.toggle('hidden');
-  if (!nav.classList.contains('hidden')) {
-    toggleNav();
+  if (nav.classList.contains('hidden')) {
+    openMenu()
+    nav.classList.remove('hidden')
   } else {
-    reverseNav();
+    closeMenu()
+    nav.classList.add('hidden')
   }
 });
 
-const toggleNav = () => {
+const openMenu = () => {
   anime({
     targets: 'nav',
     opacity: 1,
-    duration: 0,
+    duration: 500,
   });
-
-  anime({
-    targets: '.button-nav',
-    backgroundColor: '#7fffd4',
-    easing: 'easeInOutQuad',
-    rotate: 135,
-    duration: 700,
-  });
-
-  anime({
-    targets: '.button-nav div',
-    backgroundColor: 'hsla(0,0%,10%,1)',
-    easing: 'easeInOutQuad',
-    duration: 200,
-  });
-
-  anime({
-    targets: '.button-nav div:nth-child(1)',
-    rotate: 90,
-    translateX: 8,
-    translateY: 0,
-    easing: 'easeInOutQuad',
-    duration: 200,
-  });
-
-  anime({
-    targets: '.button-nav div:nth-child(2)',
-    opacity: 0,
-    easing: 'easeInOutQuad',
-    duration: 200,
-  });
-
-  anime({
-    targets: '.button-nav div:nth-child(3)',
-    rotate: 180,
-    translateX: 0,
-    translateY: 8,
-    easing: 'easeInOutQuad',
-    duration: 200,
-  });
-
   anime({
     targets: 'nav ul li',
     translateX: 48,
     opacity: 1,
-    color: '#7fffd4',
-    delay: anime.stagger(100), // increase delay by 100ms for each elements.
+    delay: anime.stagger(200)
   });
-};
+  anime({
+    targets: '.button-nav',
+    backgroundColor: '#7fffd4',
+    duration: 500
+  });
+  const button = document.querySelector('.button-nav img')
+  button.setAttribute("src", "./images/close_black_24dp.svg")
+}
+
+const closeMenu = () => {
+  anime({
+    targets: 'nav',
+    opacity: 0,
+    duration: 500,
+  });
+  anime({
+    targets: 'nav ul li',
+    translateX: -48,
+    opacity: 0,
+  });
+  anime({
+    targets: '.button-nav',
+    backgroundColor: 'hsla(0,0%,10%,1)',
+    duration: 0
+  });
+  const button = document.querySelector('.button-nav img')
+  button.setAttribute("src", "./images/menu_white_24dp.svg")
+}
